737131f6858a59caa99d5cea0b529aa8f5f131df
254801 <254801@edu.p.lodz.pl>
2026-03-14 17:02:54 +0100

    Added Ball data storage class

---
+++ ConcurrentProgramming.Data/Ball.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcurrentProgramming.Data
+{
+    public class Ball
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Ball(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Ball() { }
+    }
+}
