82e4ba7af4dc0e942f383b1cda7000e347161356
254801 <254801@edu.p.lodz.pl>
2026-03-14 19:07:26 +0100

    Added some viewmodel code

---
+++ ConcurrentProgramming.ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ConcurrentProgramming.Data;
+using ConcurrentProgramming.Logic;
+using System.Collections.ObjectModel;
+
+namespace ConcurrentProgramming.ViewModel
+{
+    public class MainViewModel
+    {
+        public readonly ObservableCollection<Ball> Balls = [];
+
+        public void AddBalls(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Balls.Add(Utils.CreateBall());
+            }
+        }
+    }
+}
