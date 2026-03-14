fa27658da68fc4eed7b91852498ffcc651b9943f
254801 <254801@edu.p.lodz.pl>
2026-03-14 18:39:20 +0100

    Added utility class to logic project

---
+++ ConcurrentProgramming.Logic/Utils.cs
@@ -0,0 +1,15 @@
+﻿using ConcurrentProgramming.Data;
+using System;
+
+namespace ConcurrentProgramming.Logic
+{
+    public static class Utils
+    {
+        private static readonly Random random = new();
+
+        public static Ball CreateBall()
+        {
+            return new Ball(random.NextDouble(), random.NextDouble());
+        }
+    }
+}
