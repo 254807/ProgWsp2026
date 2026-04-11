7ad6f87492829923ed63975cb7553795bc01584d
254801 <254801@edu.p.lodz.pl>
2026-04-11 14:18:22 +0200

    Added basic test to Logic module

---
+++ ConcurrentProgramming.Logic.Tests/BallLogicTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ConcurrentProgramming.Logic.Tests;
+
+/// <summary>
+/// The ball logic tests.
+/// </summary>
+public class BallLogicTests
+{
+    /// <summary>
+    /// Adding balls tests.
+    /// </summary>
+    [Fact]
+    public void AddBallsTests()
+    {
+        var ballLogic = new BallLogic();
+        int numberOfBalls = 10;
+
+        ballLogic.AddBalls(numberOfBalls);
+
+        Assert.Equal(numberOfBalls, ballLogic.Balls.Count);
+    }
+}
